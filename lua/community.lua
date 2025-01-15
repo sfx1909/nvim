c707527fb9a1da935ef42be242d2c2415896eb9d
Sean <seanfvdm@gmail.com>
2025-01-15 10:22:41 +0200

    Updated plugins an setup

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,44 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.color.tint-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
+
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+
+  { import = "astrocommunity.completion.coq_nvim" },
+
+  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  { import = "astrocommunity.docker.lazydocker" },
+
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.bigfile-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+  { import = "astrocommunity.editing-support.text-case-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
+
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+
+  { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
+
+  { import = "astrocommunity.recipes.vscode" },
+
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  -- { import = "astrocommunity.workflow.bad-practices-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.workflow.precognition-nvim" }
 }
